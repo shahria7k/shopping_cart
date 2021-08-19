3b790465e385042acf8bc370ef64e05684de5034
shahria jaman khan <41187226+shahria7k@users.noreply.github.com>
2021-08-19 21:14:59 +0600

    initial push

---
+++ app.js
@@ -0,0 +1,41 @@
+let removeItem = document.querySelectorAll('.remove-item');
+let increaseBtn = document.querySelectorAll('.fa-plus');
+let decreaseBtn = document.querySelectorAll('.fa-minus');
+let btns = [...increaseBtn, ...decreaseBtn];
+
+for (let btn of btns) {
+    btn.parentElement.addEventListener('click', e => {
+        try {
+            if (e.target.lastChild.classList.value.includes("fa-plus")) {
+                let count = e.target.parentElement.children[1].value;
+                e.target.parentElement.children[1].value = ++count;
+                console.log(e.target.parentElement.parentElement.children[1].lastChild.innerText);
+
+            } else {
+                let count = e.target.parentElement.children[1].value;
+                if (count > 0) {
+                    e.target.parentElement.children[1].value = --count;
+                }
+
+            }
+        } catch (error) {
+            if (e.target.parentElement.lastChild.classList.value.includes("fa-plus")) {
+                let count = e.target.parentElement.parentElement.children[1].value;
+                e.target.parentElement.parentElement.children[1].value = ++count;
+                console.log(e.target.parentElement.parentElement.parentElement.children[1].lastChild.innerText);
+            } else {
+                let count = e.target.parentElement.parentElement.children[1].value;
+                if (count > 0) {
+                    e.target.parentElement.parentElement.children[1].value = --count;
+                }
+
+            }
+        }
+
+    });
+}
+for (let item of removeItem) {
+    item.addEventListener('click', e => {
+        console.log(e.target.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement));
+    });
+}
